a7fc28428371b5d91f61aa3dd7cbdc12c0978e5b
Documenter.jl <documenter@juliadocs.github.io>
2021-10-22 15:47:42 +0000

    build based on 2937edd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FractionalTransforms.jl","page":"Home","title":"FractionalTransforms.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hello there👋","category":"page"}]
+}
